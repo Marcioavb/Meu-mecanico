e99319eed3721257e7946b4699518b3de2d72d64
MARCIO MOURA DOS SANTOS <113490711+Marcioavb@users.noreply.github.com>
2025-06-03 10:54:48 -0300

    primeiro comite

---
+++ src/main/java/com/marcio/meumecanico/MeumecanicoApplication.java
@@ -0,0 +1,13 @@
+package com.marcio.meumecanico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeumecanicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeumecanicoApplication.class, args);
+	}
+
+}
