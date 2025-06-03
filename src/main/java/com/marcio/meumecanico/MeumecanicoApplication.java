57ca456ce9af0fa7008a421a29412da7603a486f
MARCIO MOURA DOS SANTOS <113490711+Marcioavb@users.noreply.github.com>
2025-06-03 11:08:27 -0300

    configuração do projeto

--- src/main/java/com/marcio/meumecanico/MeumecanicoApplication.java
+++ src/main/java/com/marcio/meumecanico/MeumecanicoApplication.java
@@ -2,12 +2,15 @@ package com.marcio.meumecanico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class MeumecanicoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeumecanicoApplication.class, args);
 	}
-
 }
